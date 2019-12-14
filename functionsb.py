8e37f24f6d484150d14ba1b391dcdf151b0cec0d
Edison <buzzedison@gmail.com>
2019-12-14 15:27:51 +0000

    adding a file

---
+++ functionsb.py
@@ -0,0 +1,14 @@
+def where(country):
+    print(f"I am from {country}")
+where('Nigeria')
+where ('Ghana')
+where ('USA')
+where('China')
+
+
+def food(fruits):
+    for y in fruits:
+        print(y)
+list = ['Pineapple', 'PawPaw', 'Orange', 'Coconut'] 
+
+food(list)
